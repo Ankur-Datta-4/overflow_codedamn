29811cf5e6d0e983148f905ed625468ccfe1a1fb
Veeresh R G <veeresh.ravi@gmail.com>
2022-06-04 20:39:23 +0530

    :tada: PostCompo

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Navi from './Components/Navbar';
 
 import Login from './Pages/Login';
 import SignUp from './Pages/Register';
+import PostCompi from './Components/PostComponent';
 
 function App() {
 
@@ -19,12 +20,13 @@ function App() {
     <div className="App">
       <Navi />
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<SignUp/>} />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/test" element={<PostCompi />} />
 
-      </Routes>
+        </Routes>
       </BrowserRouter>
     </div>
   )
