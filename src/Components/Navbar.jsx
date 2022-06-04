d216bbcdcf2b0455eb0426c8e175414d09a96a76
Veeresh R G <veeresh.ravi@gmail.com>
2022-06-04 15:14:35 +0530

    :boom: NavBar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,45 +8,43 @@ import { Nav } from 'react-bootstrap';
 import { NavDropdown } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import { FormControl } from 'react-bootstrap';
-
+import { FcSearch } from 'react-icons/fc';
 const Navi = () => {
     return (
         <div className="navbar" style={{ width: "100%", zIndex: "2", position: "sticky", padding: "0", top: "0" }}>
             {/* <Navbar {...props} /> */}
-            <Navbar style={{ width: "100%" }} bg="light" expand="md">
+            <Navbar style={{ width: "100%", backgroundColor: "#000", color: "white" }} expand="md">
                 <Container fluid>
-                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav
-                            className="me-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
-                            <Nav.Link href="#action1">Home</Nav.Link>
-                            <Nav.Link href="#action2">Link</Nav.Link>
+                    <Navbar.Brand style={{ color: "white", fontSize: "150%", marginRight: "30px" }} href="/">PESUOVER_TEST</Navbar.Brand>
 
-                            <NavDropdown title="Link" id="navbarScrollingDropdown">
-                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action5">
-                                    Something else here
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="#" disabled>
-                                Link
-                            </Nav.Link>
-                        </Nav>
+                    <Navbar.Toggle style={{ backgroundColor: "#ffffff", color: "white" }} aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
                         <Form className="d-flex">
                             <FormControl
+                                style={{}}
                                 type="search"
                                 placeholder="Search"
                                 className="me-2"
                                 aria-label="Search"
+
                             />
-                            <Button variant="outline-success">Search</Button>
+
+                            {/* <FcSearch style={{ width: "23%", height: "23%", backgroundColor: "whitesmoke", borderRadius: "10px", marginRight: "10%" }} /> */}
+                            <Button style={{ marginLeft: "1%", marginRight: "10%" }} variant="outline-light"><FcSearch /></Button>
                         </Form>
+                        <Nav
+
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px', color: "white" }}
+                            navbarScroll
+                        >
+                            <Nav.Link style={{ color: "white", fontSize: "120%", marginRight: "13%" }} href="#action1">Home</Nav.Link>
+                            <Nav.Link style={{ color: "white", fontSize: "120%", marginRight: "13%" }} href="#action2">About</Nav.Link>
+                            <Nav.Link style={{ color: "white", fontSize: "120%", marginRight: "13%" }} href="#action2">Contact</Nav.Link>
+                            <Nav.Link style={{ color: "white", fontSize: "120%", marginRight: "13%" }} href="#action2">Link</Nav.Link>
+
+                        </Nav>
+
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
